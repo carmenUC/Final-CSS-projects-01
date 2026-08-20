e6237162192d351e24261cdf974c3ac24be7a675
Galactic Glitter <galactic_glitter@MacBook-Air-de-Galactic.local>
2026-08-20 14:21:45 -0700

    corrigiendo la configuracion de gh-pages y vite!

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://dcortesnet.github.io/Reactjs-deploy-githubpages', // Link page
+  base: '/Final-CSS-projects-01/', // Link page
 })
